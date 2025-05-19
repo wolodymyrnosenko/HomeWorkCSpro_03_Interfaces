a5a1ead22f55c4ffa7c3038649f795ca5edbb33f
volodymyr <vovanose@ukr.net>
2025-05-19 14:43:16 +0300

    Add task 03_1 IOutPut and MyArray

---
+++ HomeWorkCSpro_03_1_IOutPut/HomeWorkCSpro_03_1_IOutPut/IOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HomeWorkCSpro_03_1_IOutPut
+{
+    public interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+}
---
+++ HomeWorkCSpro_03_1_IOutPut/HomeWorkCSpro_03_1_IOutPut/MyArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWorkCSpro_03_1_IOutPut
+{
+    internal class MyArray : IOutput
+    {
+        private int[] Array { get; set; }
+
+        public MyArray(int[] array)
+        {
+            Array = array;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int item in Array)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine($"--------------------------------------{Environment.NewLine}{info}");
+            Show();
+        }
+    }
+}
